4f7f7426a44b05b57b56177f61a54e28f67d1cb7
JamesMcIlwaine-Evelyn <108079083+JamesMcIlwaine-Evelyn@users.noreply.github.com>
2023-12-24 18:44:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    return {
+        'interestInCopilot':null,
+        'opinionOfGenerativeAI':'exceedingly poor',
+        'intentionToSignUpMyPersonalAccount':'Ha'
+    };
+}
